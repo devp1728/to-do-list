bdfd649f5d849d986ba7116d2e953de2764dd76b
devp1728 <526DPatel@frhsd.com>
2025-04-06 11:12:03 -0700

    fixed javascript, added counter, fonts/pictures, modified html

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 //init on empty array element called tasks
-let tasks = []
+let tasks = [];
+
 //add on click event listener
-document.getElementById('addTaskBtn').addEventListener('click' , function ()(
+document.getElementById('addTaskBtn').addEventListener('click', function () {
     //get value of input box and store in variable called taskInput
-    let taskInput = document.GetElementById('taskInput').value
+    let taskInput = document.getElementById('taskInput').value;
 
-//check if taskInput has value
-if(taskInput){
-    tasks.push(taskInput)
-
-    document.getElementById('taskInput').value = ''
-
-    displayTasks()
-}
-
-console.log(tasks)
-
-))
+    //check if taskInput has value
+    if (taskInput) {
+        tasks.push(taskInput);
+        document.getElementById('taskInput').value = '';
+        displayTasks();
+    }
+});
 
 //function to display list
 function displayTasks() {
-    let taskList = document.getElementById('taskList')
+    let taskList = document.getElementById('taskList');
+    taskList.innerHTML = '';
 
-    taskList.innerHTML = ''
-
-
-    task.forEach((task, index) => {
-let li = document.createElement('li')
-
-li.classList.add(
-    "list-group-item",
-    "d-flex"
-    "justify-content-between"
-    "align-items-center"
-
-
-
-);
-li.innerHtml = `$(tasks) <button class='btn btn-dark btn-sm' onclick= 'removeTask($(Index))'`
-    }
-    )
+    tasks.forEach((task, index) => {
+        let li = document.createElement('li');
+        li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+        li.innerHTML = `${task} <button class='btn btn-dark btn-sm' onclick='removeTask(${index})'>Remove</button>`;
+        taskList.appendChild(li);
+        taskCounter.textContent = tasks.length;
+    });
 }
+
+//function to remove a task
+function removeTask(index) {
+    tasks.splice(index, 1);
+    displayTasks();
+}
+
+//add event listener to clear all tasks
+document.getElementById('clearTaskBtn').addEventListener('click', function () {
+    tasks = [];
+    displayTasks();
+});
